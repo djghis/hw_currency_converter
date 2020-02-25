b0bca5592b0cbfd00d21d7a640a3d9320c3caeb5
djghis <djghis@hotmail.com>
2020-02-25 17:28:45 +0000

    initial commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: '#app',
+    data: {
+      currencyRates: [],
+
+    },
+    mounted(){
+      this.getCurrencies()
+    },
+    methods: {
+      getCurrencies: function(){
+        fetch("https://api.exchangeratesapi.io/latest")
+        .then(result => result.json())
+        .then(currencies => this.currencyRates = currencies)
+      }
+    }
+  })
+})
